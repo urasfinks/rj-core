e53ac780ff367d483f1dd5a94ded5f3113f9030a
urasfinks <urasfinks@yandex.ru>
2023-01-03 15:23:14 +0300

    Добавлено рекурсивное создание директорий в UtilFile

--- src/main/java/ru/jamsys/UtilFile.java
+++ src/main/java/ru/jamsys/UtilFile.java
@@ -2,28 +2,38 @@ package ru.jamsys;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
+@SuppressWarnings("unused")
 public class UtilFile {
 
+    @SuppressWarnings("unused")
     public static byte[] readBytes(String path) throws IOException {
         return Files.readAllBytes(Paths.get(path));
     }
 
-    public static void writeBytes(String path, byte[] data, FileWriteOptions fileWriteOptions) throws IOException {
+    @SuppressWarnings("unused")
+    public static void writeBytes(String pathTarget, byte[] data, FileWriteOptions fileWriteOptions) throws IOException {
+        Path path = Paths.get(pathTarget);
+        Path parent = path.getParent();
+        if(parent != null && Files.notExists(parent)) {
+            Files.createDirectories(parent);
+        }
         switch (fileWriteOptions) {
             case CREATE_OR_REPLACE:
-                Files.write(Paths.get(path), data, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
+                Files.write(Paths.get(pathTarget), data, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
                 break;
             case CREATE_OR_APPEND:
-                Files.write(Paths.get(path), data, StandardOpenOption.CREATE, StandardOpenOption.APPEND, StandardOpenOption.WRITE);
+                Files.write(Paths.get(pathTarget), data, StandardOpenOption.CREATE, StandardOpenOption.APPEND, StandardOpenOption.WRITE);
                 break;
             default:
-                Files.write(Paths.get(path), data, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+                Files.write(Paths.get(pathTarget), data, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
         }
     }
 
+    @SuppressWarnings("unused")
     public static void remove(String path) throws IOException {
         Files.delete(Paths.get(path));
     }
