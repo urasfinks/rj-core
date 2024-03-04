b65f0dd4142a06081b8972877c51ba57418794b8
sfinks <urasfinks@yandex.ru>
2024-03-05 00:31:42 +0300

    Миграция в Security

--- src/main/java/ru/jamsys/App.java
+++ src/main/java/ru/jamsys/App.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.PropertySource;
 import ru.jamsys.component.Core;
+import ru.jamsys.component.Security;
 import ru.jamsys.task.handler.LoadStatisticToInfluxDb;
 
 
@@ -16,6 +17,15 @@ public class App {
 
     public static void main(String[] args) {
         context = SpringApplication.run(App.class, args);
+        Security.init("""
+MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAi0b4fAQEzM9iVXZ93E81xYTr6t2n
+0yEmHUVJpTe4RYT6OIO8I+5hZDYDhlInG4gh2jFlsSQXwFXKUsWEyxotIQIDAQABAkAW/KUpTr5+
+ESJRKafXMymUSn5neqLmyTtRrGxdvOcIbgu5mrUqmYLpw47fBOH2kK2hMd/u8Zum+R53843vKrYZ
+AiEA+2f6oQeE0dsRwtVQ8+5PHpsxNAm7T/VzPULX4o2RAP0CIQCN0npXwHWwo9zqX8KET2tBF6dC
+l6VO02Ybw5aKNmuX9QIhAI4hk80UDiACZQEsTi8KDIr2HBQaaF5lGriIoLqBZHgVAiBkPRztwwEr
+/VWabl58x+ll04MLxUU4xqAIBaD0RWyctQIgMt6GFvwEx0V06A+W1LUeVfWDsJPm9pd+UBE0ft+q
+r+A=
+""".toCharArray());
         context.getBean(Core.class).applicationInit(LoadStatisticToInfluxDb.class);
         context.getBean(Core.class).run();
     }
