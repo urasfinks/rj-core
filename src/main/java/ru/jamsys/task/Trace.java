a6982c9f7cc6f48da276046519f167fdb6416866
urasfinks <urasfinks@yandex.ru>
2024-03-13 15:33:33 +0300

    Рефактор

--- src/main/java/ru/jamsys/task/Trace.java
+++ src/main/java/ru/jamsys/task/Trace.java
@@ -3,6 +3,7 @@ package ru.jamsys.task;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import ru.jamsys.task.handler.TaskHandler;
 
 @Getter
 @Setter
@@ -10,9 +11,9 @@ import lombok.ToString;
 public class Trace {
     long timeMs;
     TraceEvent traceEvent;
-    AbstractTaskHandler abstractTaskHandler;
+    TaskHandler<? extends Task> abstractTaskHandler;
 
-    public Trace(long timeMs, TraceEvent traceEvent, AbstractTaskHandler abstractTaskHandler) {
+    public Trace(long timeMs, TraceEvent traceEvent, TaskHandler<? extends Task> abstractTaskHandler) {
         this.timeMs = timeMs;
         this.traceEvent = traceEvent;
         this.abstractTaskHandler = abstractTaskHandler;
