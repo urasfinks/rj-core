49daee6c0dc25cec0368ec1d345fb92a25e0d68a
urasfinks <urasfinks@yandex.ru>
2024-03-13 15:32:30 +0300

    Рефактор

--- src/main/java/ru/jamsys/task/TaskHandlerStatistic.java
+++ src/main/java/ru/jamsys/task/TaskHandlerStatistic.java
@@ -6,6 +6,7 @@ import org.springframework.lang.Nullable;
 import ru.jamsys.App;
 import ru.jamsys.broker.BrokerCollectible;
 import ru.jamsys.component.Broker;
+import ru.jamsys.task.handler.TaskHandler;
 import ru.jamsys.util.Util;
 
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -15,35 +16,40 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class TaskHandlerStatistic implements BrokerCollectible {
     Thread thread;
     Task task;
-    AbstractTaskHandler taskHandler;
+    TaskHandler<? extends Task> taskHandler;
 
     long timeStartMs = System.currentTimeMillis();
     Long timeExecuteMs = null;
 
     AtomicBoolean firstPrepare = new AtomicBoolean(true); //Было обработано статистикой
 
+    @SuppressWarnings("unused")
     public boolean isFirstPrepare() {
         return firstPrepare.compareAndSet(true, false);
     }
 
-    public TaskHandlerStatistic(Thread thread, @Nullable Task task, AbstractTaskHandler abstractTaskHandler) {
+    public TaskHandlerStatistic(Thread thread, @Nullable Task task, TaskHandler<? extends Task> abstractTaskHandler) {
         this.thread = thread;
         this.task = task;
         this.taskHandler = abstractTaskHandler;
     }
 
+    @SuppressWarnings("unused")
     public Long getTimeExecuteMs() {
         return timeExecuteMs != null ? timeExecuteMs : System.currentTimeMillis() - timeStartMs;
     }
 
+    @SuppressWarnings("unused")
     public void finish() {
         timeExecuteMs = System.currentTimeMillis() - timeStartMs;
     }
 
+    @SuppressWarnings("unused")
     public boolean isTimeout() {
         return timeExecuteMs == null && (System.currentTimeMillis() - timeStartMs) > taskHandler.getTimeoutMs();
     }
 
+    @SuppressWarnings("unused")
     public boolean isFinished() {
         return timeExecuteMs != null;
     }
