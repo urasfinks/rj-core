08a7545857f95765182d1f47cc9d31105ce93f5f
urasfinks <urasfinks@yandex.ru>
2024-03-15 15:07:00 +0300

    Расширены тесты

--- src/test/java/ru/jamsys/component/TaskManagerTest.java
+++ src/test/java/ru/jamsys/component/TaskManagerTest.java
@@ -3,7 +3,6 @@ package ru.jamsys.component;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -19,7 +18,7 @@ class TaskManagerTest {
         map.put("KeepAliveTask", 2000L);
         map.put("FlushStatisticCollectorTask", 3000L);
         calc = TaskManager.calc(map, 500);
-        Assertions.assertEquals("{ReadStatisticSecTask=250, KeepAliveTask=167, FlushStatisticCollectorTask=83}", calc.toString(), "#1");
+        Assertions.assertEquals("{ReadStatisticSecTask=272, KeepAliveTask=136, FlushStatisticCollectorTask=90}", calc.toString(), "#1");
 
         map.clear();
         map.put("KeepAliveTask", 0L);
@@ -38,6 +37,13 @@ class TaskManagerTest {
         map.put("KeepAliveTask", 2L);
         map.put("FlushStatisticCollectorTask", 2L);
         calc = TaskManager.calc(map, 500);
-        Assertions.assertEquals("{ReadStatisticSecTask=233, KeepAliveTask=133, FlushStatisticCollectorTask=133}", calc.toString(), "#3");
+        Assertions.assertEquals("{ReadStatisticSecTask=250, KeepAliveTask=125, FlushStatisticCollectorTask=125}", calc.toString(), "#3");
+
+        map.clear();
+        map.put("ReadStatisticSecTask", 0L);
+        map.put("KeepAliveTask", 506L);
+        map.put("FlushStatisticCollectorTask", 1L);
+        calc = TaskManager.calc(map, 500);
+        Assertions.assertEquals("{ReadStatisticSecTask=249, KeepAliveTask=1, FlushStatisticCollectorTask=249}", calc.toString(), "#3");
     }
 }
