ad45b51359d6f560bb4fe5f707a1438b638c2f04
sfinks <urasfinks@yandex.ru>
2024-03-16 22:49:30 +0300

    + @SuppressWarnings("unused")

--- src/main/resources/SecurityKey.java
+++ src/main/resources/SecurityKey.java
@@ -3,9 +3,11 @@ package ru.jamsys;
 import org.springframework.stereotype.Component;
 import ru.jamsys.component.Security;
 
+@SuppressWarnings("unused")
 @Component
 public class SecurityKey {
 
+    @SuppressWarnings("unused")
     public SecurityKey(Security security) {
         security.setPrivateKey("""
 {privateKey}
