511211709770efc8edeff3431ffdf5347b4ab658
sfinks <urasfinks@yandex.ru>
2024-03-19 21:04:47 +0300

    Косяк типов после миграции

--- src/main/java/ru/jamsys/component/TaskManager.java
+++ src/main/java/ru/jamsys/component/TaskManager.java
@@ -1,7 +1,6 @@
 package ru.jamsys.component;
 
 import org.springframework.stereotype.Component;
-import ru.jamsys.broker.QueueElementEnvelope;
 import ru.jamsys.extension.KeepAliveComponent;
 import ru.jamsys.extension.StatisticsCollectorComponent;
 import ru.jamsys.statistic.AvgMetric;
@@ -74,11 +73,10 @@ public class TaskManager implements KeepAliveComponent, StatisticsCollectorCompo
                             if (count > maxCountIteration) {
                                 return false;
                             }
-                            QueueElementEnvelope<Task> queueElementEnvelope = broker.pollLast(index);
-                            if (queueElementEnvelope == null) {
+                            Task task = broker.pollLast(index);
+                            if (task == null) {
                                 return false;
                             }
-                            Task task = queueElementEnvelope.getElement();
                             @SuppressWarnings("unchecked")
                             Handler<Task> handler = dictionary.getTaskHandler().get(task.getClass());
                             if (handler != null) {
