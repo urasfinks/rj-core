a075b0cba3c5af7bf333825e50deb39863432315
sfinks <urasfinks@yandex.ru>
2024-03-19 22:41:18 +0300

    Расширение вывода исключения

--- src/main/java/ru/jamsys/component/ExceptionHandler.java
+++ src/main/java/ru/jamsys/component/ExceptionHandler.java
@@ -3,12 +3,20 @@ package ru.jamsys.component;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 @Component
 @Lazy
 public class ExceptionHandler {
 
     public void handler(Exception e) {
-        e.printStackTrace();
+        StringWriter sw = new StringWriter();
+        sw.append(Thread.currentThread().getName());
+        sw.append("\n");
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        System.err.println(sw);
     }
 
 }
