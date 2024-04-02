17fa2608fbe635c2833dd8e027442d2c4a62c0b2
sfinks <urasfinks@yandex.ru>
2024-04-02 21:10:03 +0300

    Набросок компонента Cache

---
+++ src/main/java/ru/jamsys/component/CacheManager.java
@@ -0,0 +1,41 @@
+package ru.jamsys.component;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+import ru.jamsys.cache.Cache;
+import ru.jamsys.extension.KeepAliveComponent;
+import ru.jamsys.extension.StatisticsCollectorComponent;
+import ru.jamsys.statistic.Statistic;
+import ru.jamsys.util.Util;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@SuppressWarnings("unused")
+@Component
+@Lazy
+public class CacheManager implements KeepAliveComponent, StatisticsCollectorComponent {
+
+    Map<String, Cache<?, ?>> map = new ConcurrentHashMap<>();
+
+    @Override
+    public void keepAlive(AtomicBoolean isRun) {
+        Util.riskModifierMap(isRun, map, new String[0], (String key, Cache<?, ?> cache) -> cache.keepAlive(isRun));
+    }
+
+    @Override
+    public List<Statistic> flushAndGetStatistic(Map<String, String> parentTags, Map<String, Object> parentFields, AtomicBoolean isRun) {
+        List<Statistic> result = new ArrayList<>();
+        Util.riskModifierMap(isRun, map, new String[0], (String key, Cache<?, ?> cache) -> {
+            Map<String, String> parentTagsNew = new LinkedHashMap<>(parentTags);
+            parentTagsNew.put("KeyCache", key);
+            result.addAll(cache.flushAndGetStatistic(parentTagsNew, parentFields, isRun));
+        });
+        return result;
+    }
+
+}
