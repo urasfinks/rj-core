29d08e5d98c02dc68c965b6bfb93608285e91c9e
sfinks <urasfinks@yandex.ru>
2024-05-11 11:43:26 +0300

    Trend -> UtilTrend

--- src/test/java/ru/jamsys/core/component/UtilTrendTest.java
+++ src/test/java/ru/jamsys/core/component/UtilTrendTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.jamsys.core.App;
-import ru.jamsys.core.component.api.Trend;
+import ru.jamsys.core.util.UtilTrend;
 
-class TrendTest {
+class UtilTrendTest {
     @BeforeAll
     static void beforeAll() {
         String[] args = new String[]{};
@@ -15,7 +15,7 @@ class TrendTest {
 
     @Test
     void getPoly() {
-        Trend bean = App.context.getBean(Trend.class);
+        UtilTrend bean = App.context.getBean(UtilTrend.class);
         Assertions.assertEquals(28, Math.round(bean.getPoly(2, new double[]{13, 25, 98, 40, 15, 66})), "#1");
         Assertions.assertEquals(160, Math.round(bean.getPoly(3, new double[]{13, 25, 98, 40, 15, 66})), "#2");
         Assertions.assertEquals(512, Math.round(bean.getPoly(4, new double[]{13, 25, 98, 40, 15, 66})), "#3");
@@ -25,7 +25,7 @@ class TrendTest {
 
     @Test
     void getPredictPoly() {
-        Trend bean = App.context.getBean(Trend.class);
+        UtilTrend bean = App.context.getBean(UtilTrend.class);
         Assertions.assertEquals(5, Math.round(bean.getPoly(2, new double[]{1, 2, 3, 4})), "#1");
         Assertions.assertEquals(5, Math.round(bean.getPoly(2, new double[]{1, 2, 3, 4}, 1)), "#2");
         Assertions.assertEquals(6, Math.round(bean.getPoly(2, new double[]{1, 2, 3, 4}, 2)), "#3");
@@ -35,7 +35,7 @@ class TrendTest {
 
     @Test
     void getLinear() {
-        Trend bean = App.context.getBean(Trend.class);
+        UtilTrend bean = App.context.getBean(UtilTrend.class);
         Assertions.assertEquals(6, Math.round(bean.getLinear(new double[]{1, 2, 3, 4, 5}, 1)), "#1");
         Assertions.assertEquals(7, Math.round(bean.getLinear(new double[]{1, 2, 3, 4, 5}, 2)), "#2");
         Assertions.assertEquals(-14419, Math.round(bean.getLinear(new double[]{74311, 37555, 30541, 25938, 30490, 6853, 1523, 1522}, 1)), "#3");
