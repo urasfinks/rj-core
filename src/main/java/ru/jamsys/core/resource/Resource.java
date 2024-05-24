e7b5853c6322284a247df078b8d24811f8028dda
sfinks <urasfinks@yandex.ru>
2024-05-24 23:00:02 +0300

    Коммент

--- src/main/java/ru/jamsys/core/resource/Resource.java
+++ src/main/java/ru/jamsys/core/resource/Resource.java
@@ -2,6 +2,9 @@ package ru.jamsys.core.resource;
 
 import ru.jamsys.core.resource.balancer.BalancerItem;
 
+// A - Arguments
+// R - Result
+
 public interface Resource<A, R> extends BalancerItem {
 
     // Class<R> clsRes снимаем ответственность на unchecked
