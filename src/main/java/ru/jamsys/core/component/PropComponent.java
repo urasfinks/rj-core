53a8f0be5f5c95e089dd4ce19da05a27d45bb430
sfinks <urasfinks@yandex.ru>
2024-06-07 19:16:29 +0300

    Начало в централизованное хранилище

--- src/main/java/ru/jamsys/core/component/PropComponent.java
+++ src/main/java/ru/jamsys/core/component/PropComponent.java
@@ -2,12 +2,13 @@ package ru.jamsys.core.component;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
@@ -19,8 +20,28 @@ public class PropComponent {
 
     final Map<String, List<Consumer>> subscribe = new ConcurrentHashMap<>();
 
+    Map<String, Object> prop = new HashMap<>();
+
     public PropComponent(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
+        Environment env = applicationContext.getEnvironment();
+        MutablePropertySources propertySources = ((AbstractEnvironment) env).getPropertySources();
+        for (org.springframework.core.env.PropertySource<?> next : propertySources) {
+            String name = next.getName();
+            if (
+                    name.equals("configurationProperties")
+                            || name.equals("systemProperties")
+                            || name.equals("systemEnvironment")
+                            || name.equals("random")
+            ) {
+                continue;
+            }
+            if (next instanceof EnumerablePropertySource) {
+                for (String prop : ((EnumerablePropertySource<?>) next).getPropertyNames()) {
+                    this.prop.put(prop, env.getProperty(prop));
+                }
+            }
+        }
     }
 
     public <T> void getProp(String namespace, String key, Class<T> cls, Consumer<T> onUpdate) {
@@ -28,8 +49,8 @@ public class PropComponent {
     }
 
     public <T> void getProp(String key, Class<T> cls, Consumer<T> onUpdate) {
-        Environment environment = applicationContext.getEnvironment();
-        T result = environment.getProperty(key, cls);
+        @SuppressWarnings("unchecked")
+        T result = (T) prop.get(key);
         if (result == null) {
             throw new RuntimeException("Required key '" + key + "' not found");
         }
