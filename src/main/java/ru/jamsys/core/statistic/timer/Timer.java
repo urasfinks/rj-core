3548ecb4f34bf8fee74fd39184b82e460a749687
sfinks <urasfinks@yandex.ru>
2024-06-19 22:07:58 +0300

    Нет места комментам

--- src/main/java/ru/jamsys/core/statistic/timer/Timer.java
+++ src/main/java/ru/jamsys/core/statistic/timer/Timer.java
@@ -2,8 +2,6 @@ package ru.jamsys.core.statistic.timer;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
-import ru.jamsys.core.flat.util.Util;
-import ru.jamsys.core.statistic.timer.ms.TimerMsEnvelope;
 import ru.jamsys.core.statistic.timer.nano.TimerNanoEnvelope;
 
 @Getter
@@ -12,32 +10,12 @@ public class Timer {
 
     private final TimerNanoEnvelope<String> nano;
 
-    private final TimerMsEnvelope<Void> ms;
-
     public Timer(String index) {
         this.nano = new TimerNanoEnvelope<>(index);
-        this.ms = new TimerMsEnvelope<>(null);
     }
 
     public void stop() {
         nano.stop();
-        ms.stop();
-    }
-
-    public String getStart() {
-        return Util.msToDataFormat(ms.getLastActivityMs());
-    }
-
-    public String getStop() {
-        Long timeStopMs = ms.getTimeStopMs();
-        if (timeStopMs != null) {
-            return Util.msToDataFormat(timeStopMs);
-        }
-        return null;
-    }
-
-    public long getMs() {
-        return ms.getOffsetLastActivityMs();
     }
 
     public long getNano() {
