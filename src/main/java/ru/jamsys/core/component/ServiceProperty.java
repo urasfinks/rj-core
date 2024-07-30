8e70649410433365859f608b0acb8f0aaf765d5a
urasfinks <urasfinks@yandex.ru>
2024-07-30 19:23:19 +0300

    Начало подписки

--- src/main/java/ru/jamsys/core/component/ServiceProperty.java
+++ src/main/java/ru/jamsys/core/component/ServiceProperty.java
@@ -22,7 +22,9 @@ import java.util.concurrent.ConcurrentHashMap;
 @Lazy
 public class ServiceProperty {
 
-    final private Map<String, Set<PropertyUpdateDelegate>> subscribe = new ConcurrentHashMap<>();
+    final private Map<String, Set<PropertyUpdateDelegate>> subscriberKey = new ConcurrentHashMap<>();
+
+    final private Map<String, Set<PropertyUpdateDelegate>> subscriberContains = new ConcurrentHashMap<>();
 
     @Getter
     final private ServicePropertyFactory factory;
@@ -59,20 +61,19 @@ public class ServiceProperty {
     public void setProperty(Map<String, String> map) {
         Set<PropertyUpdateDelegate> notify = new HashSet<>();
         if (!map.isEmpty()) {
-            for (String key : map.keySet()) {
-                String value = map.get(key);
+            map.forEach((key, value) -> {
                 if (value == null) {
                     prop.remove(key);
-                    if (subscribe.containsKey(key)) {
-                        notify.addAll(subscribe.get(key));
+                    if (subscriberKey.containsKey(key)) {
+                        notify.addAll(subscriberKey.get(key));
                     }
                 } else if (!prop.containsKey(key) || !prop.get(key).equals(value)) {
                     prop.put(key, value);
-                    if (subscribe.containsKey(key)) {
-                        notify.addAll(subscribe.get(key));
+                    if (subscriberKey.containsKey(key)) {
+                        notify.addAll(subscriberKey.get(key));
                     }
                 }
-            }
+            });
             notify.forEach(subscriber -> subscriber.onPropertyUpdate(new HashMap<>(map)));
         }
     }
@@ -85,12 +86,12 @@ public class ServiceProperty {
         if (result == null) {
             result = prop.computeIfAbsent(absoluteKey, _ -> defValue);
         }
-        subscribe.computeIfAbsent(absoluteKey, _ -> new HashSet<>()).add(propertyUpdateDelegate);
+        subscriberKey.computeIfAbsent(absoluteKey, _ -> new HashSet<>()).add(propertyUpdateDelegate);
         propertyUpdateDelegate.onPropertyUpdate(new HashMapBuilder<String, String>().append(absoluteKey, result));
     }
 
     public void unsubscribe(String absoluteKey, PropertyUpdateDelegate propertyUpdateDelegate) {
-        subscribe.get(absoluteKey).remove(propertyUpdateDelegate);
+        subscriberKey.get(absoluteKey).remove(propertyUpdateDelegate);
     }
 
 }
