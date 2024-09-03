7aacb147e70dbca1f9bc40909b6dc191cd77a293
sfinks <urasfinks@yandex.ru>
2024-09-03 17:18:01 +0300

    Рефактор EmailNotification

--- src/main/java/ru/jamsys/core/resource/notification/email/EmailTemplateNotificationRequest.java
+++ src/main/java/ru/jamsys/core/resource/notification/email/EmailTemplateNotificationRequest.java
@@ -3,6 +3,7 @@ package ru.jamsys.core.resource.notification.email;
 import ru.jamsys.core.App;
 import ru.jamsys.core.extension.exception.ForwardException;
 import ru.jamsys.core.flat.template.twix.TemplateTwix;
+import ru.jamsys.core.flat.util.Util;
 import ru.jamsys.core.flat.util.UtilFileResource;
 
 import java.util.Map;
@@ -19,10 +20,14 @@ public class EmailTemplateNotificationRequest extends EmailNotificationRequest {
     ) {
         super(title, data, "", to);
         try {
-            ClassLoader classLoader = templateClassLoader.equals("rj-core")
-                    ? App.class.getClassLoader()
-                    : ClassLoader.getSystemClassLoader();
-            String tpl = UtilFileResource.getAsStringByClassLoader(classLoader, templatePath);
+            String tpl;
+            if (templateClassLoader.equals("web")) {
+                tpl = Util.getWebContent(templatePath);
+            } else if (templateClassLoader.equals("rj-core")) {
+                tpl = UtilFileResource.getAsStringByClassLoader(App.class.getClassLoader(), templatePath);
+            } else {
+                tpl = UtilFileResource.getAsStringByClassLoader(ClassLoader.getSystemClassLoader(), templatePath);
+            }
             setDataHtml(TemplateTwix.template(tpl, args));
         } catch (Exception e) {
             throw new ForwardException(e);
