c7af7516ac3c60db25f87e19cd97ac4a341e1c11
sfinks <urasfinks@yandex.ru>
2024-10-06 01:12:43 +0300

    Selection.Add

--- src/main/java/ru/jamsys/core/i360/entity/adapter/selection/Add.java
+++ src/main/java/ru/jamsys/core/i360/entity/adapter/selection/Add.java
@@ -1,12 +1,21 @@
 package ru.jamsys.core.i360.entity.adapter.selection;
 
 import ru.jamsys.core.i360.Context;
+import ru.jamsys.core.i360.entity.Entity;
+
+import java.util.List;
 
 public class Add implements ContextSelection {
 
     @Override
     public Context transform(Context context, Context contextSelection) {
-        return null;
+        List<Entity> contextEntity = context.getListEntity();
+        List<Entity> contextSelectionEntity = contextSelection.getListEntity();
+        Context result = new Context();
+        List<Entity> listEntityResult = result.getListEntity();
+        listEntityResult.addAll(contextEntity);
+        listEntityResult.addAll(contextSelectionEntity);
+        return listEntityResult.isEmpty() ? null : result;
     }
 
 }
---
+++ src/test/java/ru/jamsys/core/i360/entity/adapter/selection/AddTest.java
@@ -0,0 +1,29 @@
+package ru.jamsys.core.i360.entity.adapter.selection;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.jamsys.core.i360.Context;
+import ru.jamsys.core.i360.entity.EntityImpl;
+
+class AddTest {
+
+    @Test
+    public void test() {
+        // Добавить ["1", "2"] mask ["3"] => ["1", "2", "3"]
+        Context input = new Context();
+        input.getListEntity().add(new EntityImpl(null, "1"));
+        input.getListEntity().add(new EntityImpl(null, "2"));
+
+        Context selection = new Context();
+        selection.getListEntity().add(new EntityImpl(null, "3"));
+
+        Context result = new Context();
+        result.getListEntity().add(new EntityImpl(null, "1"));
+        result.getListEntity().add(new EntityImpl(null, "2"));
+        result.getListEntity().add(new EntityImpl(null, "3"));
+
+        Assertions.assertEquals(result, new Add().transform(input, selection));
+
+    }
+
+}
