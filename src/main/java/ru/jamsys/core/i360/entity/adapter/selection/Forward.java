3a3f9cac23b35939a105e01da0b846c8cd7bab0b
sfinks <urasfinks@yandex.ru>
2024-10-06 00:38:30 +0300

    Selection.Forward

--- src/main/java/ru/jamsys/core/i360/entity/adapter/selection/Forward.java
+++ src/main/java/ru/jamsys/core/i360/entity/adapter/selection/Forward.java
@@ -1,12 +1,24 @@
 package ru.jamsys.core.i360.entity.adapter.selection;
 
 import ru.jamsys.core.i360.Context;
+import ru.jamsys.core.i360.entity.Entity;
+
+import java.util.List;
 
 public class Forward implements ContextSelection {
 
     @Override
     public Context transform(Context context, Context contextSelection) {
-        return null;
+        List<Entity> contextEntity = context.getListEntity();
+        List<Entity> contextSelectionEntity = contextSelection.getListEntity();
+        Context result = new Context();
+        List<Entity> listEntityResult = result.getListEntity();
+        contextEntity.forEach(entity -> {
+            if (contextSelectionEntity.contains(entity)) {
+                listEntityResult.add(entity);
+            }
+        });
+        return listEntityResult.isEmpty() ? null : result;
     }
 
 }
