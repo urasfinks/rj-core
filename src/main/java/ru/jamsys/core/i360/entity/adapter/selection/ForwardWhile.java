549d654e32b6b650cb289cd5a082c556b627185d
sfinks <urasfinks@yandex.ru>
2024-10-06 00:44:56 +0300

    Selection.ForwardWhile

--- src/main/java/ru/jamsys/core/i360/entity/adapter/selection/ForwardWhile.java
+++ src/main/java/ru/jamsys/core/i360/entity/adapter/selection/ForwardWhile.java
@@ -1,12 +1,26 @@
 package ru.jamsys.core.i360.entity.adapter.selection;
 
 import ru.jamsys.core.i360.Context;
+import ru.jamsys.core.i360.entity.Entity;
+
+import java.util.List;
 
 public class ForwardWhile implements ContextSelection {
 
     @Override
     public Context transform(Context context, Context contextSelection) {
-        return null;
+        List<Entity> contextEntity = context.getListEntity();
+        List<Entity> contextSelectionEntity = contextSelection.getListEntity();
+        Context result = new Context();
+        List<Entity> listEntityResult = result.getListEntity();
+        for (Entity entity : contextEntity) {
+            if (contextSelectionEntity.contains(entity)) {
+                listEntityResult.add(entity);
+            } else {
+                break;
+            }
+        }
+        return listEntityResult.isEmpty() ? null : result;
     }
 
 }
---
+++ src/test/java/ru/jamsys/core/i360/entity/adapter/selection/ForwardWhileTest.java
@@ -0,0 +1,30 @@
+package ru.jamsys.core.i360.entity.adapter.selection;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.jamsys.core.i360.Context;
+import ru.jamsys.core.i360.entity.EntityImpl;
+
+class ForwardWhileTest {
+
+    @Test
+    public void test() {
+        // Пробрасывать пока встречаются ["2", "1", "2", "3"] mask ["2", "3"] => ["2"]
+        Context input = new Context();
+        input.getListEntity().add(new EntityImpl(null, "2"));
+        input.getListEntity().add(new EntityImpl(null, "1"));
+        input.getListEntity().add(new EntityImpl(null, "2"));
+        input.getListEntity().add(new EntityImpl(null, "3"));
+
+        Context selection = new Context();
+        selection.getListEntity().add(new EntityImpl(null, "2"));
+        selection.getListEntity().add(new EntityImpl(null, "3"));
+
+        Context result = new Context();
+        result.getListEntity().add(new EntityImpl(null, "2"));
+
+        Assertions.assertEquals(result, new ForwardWhile().transform(input, selection));
+
+    }
+
+}
