748ec6be4aefb032b4dcc895c5294cf19df9e346
sfinks <urasfinks@yandex.ru>
2024-10-06 00:24:15 +0300

    Selection.RemoveWhile

---
+++ src/test/java/ru/jamsys/core/i360/entity/adapter/selection/RemoveWhileTest.java
@@ -0,0 +1,32 @@
+package ru.jamsys.core.i360.entity.adapter.selection;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.jamsys.core.i360.Context;
+import ru.jamsys.core.i360.entity.EntityImpl;
+
+class RemoveWhileTest {
+
+    @Test
+    public void test() {
+        // Удалять пока встречаются ["0", "0", "1", "2", "0"] mask ["0"] => ["1", "2", "0"]
+        Context input = new Context();
+        input.getListEntity().add(new EntityImpl(null, "0"));
+        input.getListEntity().add(new EntityImpl(null, "0"));
+        input.getListEntity().add(new EntityImpl(null, "1"));
+        input.getListEntity().add(new EntityImpl(null, "2"));
+        input.getListEntity().add(new EntityImpl(null, "0"));
+
+        Context selection = new Context();
+        selection.getListEntity().add(new EntityImpl(null, "0"));
+
+        Context result = new Context();
+        result.getListEntity().add(new EntityImpl(null, "1"));
+        result.getListEntity().add(new EntityImpl(null, "2"));
+        result.getListEntity().add(new EntityImpl(null, "0"));
+
+        Assertions.assertEquals(result, new RemoveWhile().transform(input, selection));
+
+    }
+
+}
