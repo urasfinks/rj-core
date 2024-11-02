5c36d40ae8e785e3e95136e859b7f25e931febd3
sfinks <urasfinks@yandex.ru>
2024-11-02 20:47:30 +0300

    +chain setter

--- src/main/java/ru/jamsys/core/i360/scale/Scale.java
+++ src/main/java/ru/jamsys/core/i360/scale/Scale.java
@@ -7,6 +7,8 @@ import lombok.Setter;
 import lombok.ToString;
 import ru.jamsys.core.i360.entity.EntityChain;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @ToString
@@ -21,5 +23,41 @@ public class Scale {
     private ScaleType type;
 
     private double stability = 0; // [0-1]
+
+    public Scale setLeft(EntityChain left) {
+        this.left = left;
+        return this;
+    }
+
+    public Scale setRight(EntityChain right) {
+        this.right = right;
+        return this;
+    }
+
+    public Scale setType(ScaleType type) {
+        this.type = type;
+        return this;
+    }
+
+    public Scale setStability(double stability) {
+        this.stability = stability;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Scale scale = (Scale) object;
+        return Double.compare(stability, scale.stability) == 0
+                && Objects.equals(left, scale.left)
+                && Objects.equals(right, scale.right)
+                && type == scale.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right, type, stability);
+    }
 
 }
