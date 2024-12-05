8037e0941f6f300c5d7686a6e8f0aef45f3295ad
sfinks <urasfinks@yandex.ru>
2024-12-05 18:39:41 +0300

    Рефактор

--- src/test/java/ru/jamsys/core/HttpRequestReaderTest.java
+++ src/test/java/ru/jamsys/core/HttpRequestReaderTest.java
@@ -2,8 +2,6 @@ package ru.jamsys.core;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 import ru.jamsys.core.extension.http.ServletRequestReader;
 
 import java.util.List;
