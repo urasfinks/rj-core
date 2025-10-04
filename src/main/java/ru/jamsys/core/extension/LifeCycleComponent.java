f7cecddf7974f1edd2d80856331d805baeddf03c
sfinks <urasfinks@yandex.ru>
2025-10-04 12:21:05 +0300

    + коммент

--- src/main/java/ru/jamsys/core/extension/LifeCycleComponent.java
+++ src/main/java/ru/jamsys/core/extension/LifeCycleComponent.java
@@ -1,6 +1,7 @@
 package ru.jamsys.core.extension;
 
-//Компонент будет запускаться автоматически при старте
+// Компонент будет запускаться автоматически при старте
+// В проектах начинайте со 100
 public interface LifeCycleComponent extends LifeCycleInterface {
     int getInitializationIndex();
 }
